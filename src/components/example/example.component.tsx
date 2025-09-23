02e40bd7b145fdf976e7bbb3c2b3b4c4facb8c71
Geodayo <george.pikachu@gmail.com>
2025-09-23 11:34:54 -0400

    react environtment

---
+++ src/components/example/example.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Example: React.FC = () => {
+  return (
+    <div className="p-3 border rounded">
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+};
+
+export default Example;

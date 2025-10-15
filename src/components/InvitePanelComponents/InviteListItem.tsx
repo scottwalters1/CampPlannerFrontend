f386dd0c1c8eb713ffcaae9a3bc6797620f9d147
Scott Walters <scottw567@gmail.com>
2025-10-15 14:40:30 -0400

    invites tweaked

--- src/components/InvitePanelComponents/InviteListItem.tsx
+++ src/components/InvitePanelComponents/InviteListItem.tsx
@@ -4,6 +4,7 @@ import { apiFetch } from "../../api/api";
 
 interface InviteListItemProps {
   invite: Invite;
+  onActionComplete: () => void;
 }
 
 interface User {
@@ -12,34 +13,37 @@ interface User {
   // add any other fields you want from the user object
 }
 
-export const InviteListItem: React.FC<InviteListItemProps> = ({ invite }) => {
+export const InviteListItem: React.FC<InviteListItemProps> = ({
+  invite,
+  onActionComplete,
+}) => {
   const [user, setUser] = useState<User | null>(null);
 
   // clear invite when accepted or denied
   useEffect(() => {
-  async function fetchUser() {
-    console.log(invite);
-    try {
-      const userId = invite.ownerId; 
-      const encodedId = encodeURIComponent(userId); 
-      const data = await apiFetch(`/users/id/${encodedId}`);
-      setUser(data);
-    } catch (err: any) {
-      console.error(err);
+    async function fetchUser() {
+      console.log(invite);
+      try {
+        const userId = invite.ownerId;
+        const encodedId = encodeURIComponent(userId);
+        const data = await apiFetch(`/users/id/${encodedId}`);
+        setUser(data);
+      } catch (err: any) {
+        console.error(err);
+      }
     }
-  }
 
     fetchUser();
   }, [invite.ownerId]);
 
   async function handleResponse(status: "Accepted" | "Denied") {
-    
     try {
       await apiFetch(`/trips/invites/${invite.tripId}`, {
         method: "PATCH",
         body: { status },
       });
       alert(`Invite ${status}.`);
+      onActionComplete();
     } catch (err: any) {
       console.error("Failed to update invite:", err);
       alert("Something went wrong updating the invite.");
@@ -53,12 +57,8 @@ export const InviteListItem: React.FC<InviteListItemProps> = ({ invite }) => {
       <p>Description: {invite.tripDescription}</p>
       <p>Invitation from: {user?.username}</p>
       <div>
-        <button onClick={() => handleResponse("Accepted")}>
-          Accept
-        </button>
-        <button onClick={() => handleResponse("Denied")}>
-          Deny
-        </button>
+        <button onClick={() => handleResponse("Accepted")}>Accept</button>
+        <button onClick={() => handleResponse("Denied")}>Deny</button>
       </div>
     </>
   );

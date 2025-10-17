97f45c25284b3bb59f604d6ac31e12b301dfc768
Scott Walters <scottw567@gmail.com>
2025-10-17 12:50:20 -0400

    import fixed

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
-import { useContext, type JSX } from "react";
+// import { useContext, type JSX } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
+import type { JSX } from "react";
 
 interface ProtectedRoutesProps{
     children: JSX.Element;

dc3c4decff23fff13d03fe90f435521fa7433c36
andrenguyen <AndrePNguyen@gmail.com>
2025-10-17 13:34:09 -0400

    !

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home = (): JSX.Element => {
   return (
     <div className="main camp-bg">
       <div style={{ backgroundColor: "#03424d8c", padding: 20, margin: 30 }} className="w-50 rounded-5">
-        <h1>Start Your Next Adventure</h1>
+        <h1>Start Your Next Adventure!</h1>
         <h6>
           Find the best campgrounds, plan thrilling activities, and enjoy every moment without worrying about logistics.
           All your trip details are in one place, so you can focus on the fun. Adventure has never been this simple.
